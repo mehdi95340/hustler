d513da36b38dcd5d079e6b6027a952c9893e1c33
dmitry goremykin <dmitrygoremykinzw@gmail.com>
2024-12-03 14:22:54 +0200

    add an expense

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,51 +1,49 @@
 class BudgetsController < ApplicationController
-    before_action :set_budget, only: [:show, :update, :edit, :destroy]
-    before_action :authenticate_user!
+  before_action :set_budget, only: [:show, :update, :edit, :destroy]
 
 
-    def index
-      @budgets = current_user.budgets
+  def index
+    @budgets = current_user.budgets
 
+  end
+
+  def show
+  end
+
+  def new
+    @budget = current_user.budgets.new
+  end
+
+  def edit
+  end
+
+  def create
+    @budget = current_user.budgets.new(budget_params)
+    if @budget.save
+      redirect_to @budget, notice: "Budget was create."
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def show
+  def update
+    if @budget.update(budget_params)
+      redirect_to @budget, notice: "budget was update"
     end
+  end
 
-    def new
-      @budget = current_user.budgets.new
-    end
+  def destroy
+    @budget.destroy
+    redirect_to budget_url, notice: "budget was destroy"
+  end
 
-    def edit
-    end
+  private
 
-    def create
-      @budget = current_user.budgets.new(budget_params)
-      if @budget.save
-        redirect_to @budget, notice: "Budget was create."
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
+  def set_budget
+    @budget = Budget.find(params[:id])
+  end
 
-    def update
-      if @budget.update(budget_params)
-        redirect_to @budget, notice: "budget was update"
-      end
-    end
-
-    def destroy
-      @budget.destroy
-      redirect_to budget_url, notice: budget was destroy
-    end
-
-    private
-
-    def set_budget
-      @budget = current_user.budgets.find(params[:id])
-    end
-
-    def budget_params
-      params.require(:budget).permit(:total_amount, :month)
-    end
+  def budget_params
+    params.require(:budget).permit(:total_amount, :month)
   end
 end
