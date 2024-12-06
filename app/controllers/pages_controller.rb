af9eda365dfe0e3b4878ebb26cc97d6d88b302ea
Grant Ndamba <grantbeven@icloud.com>
2024-12-06 15:20:41 +0200

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    # @budgets = current_user.budgets
     @current_budget = current_user.budgets.last
     @remaining_budget = @current_budget.remaining_budget
     @total_expenses = @current_budget.total_expenses
   end
 
   def dashboard
-    @budgets = currents_user.budgets
+    @budgets = current_user.budgets
     @expenses = Expense.all
   end
 
