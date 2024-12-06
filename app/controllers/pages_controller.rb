260bd499a03e978f5fc06f74936769d777ddda97
Grant Ndamba <grantbeven@icloud.com>
2024-12-06 15:41:22 +0200

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,24 @@
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    # @budgets = current_user.budgets
-    @current_budget = current_user.budgets.last
-    @remaining_budget = @current_budget.remaining_budget
-    @total_expenses = @current_budget.total_expenses
+    if current_user
+      @current_budget = current_user.budgets.last
+      if @current_budget
+        @remaining_budget = @current_budget.remaining_budget
+        @total_expenses = @current_budget.total_expenses
+      else
+        @remaining_budget = 0
+        @total_expenses = 0
+      end
+    else
+      @current_budget = nil
+      @remaining_budget = 0
+      @total_expenses = 0
+    end
   end
-
+ 
   def dashboard
     @budgets = current_user.budgets
     @expenses = Expense.all
@@ -16,3 +27,37 @@ class PagesController < ApplicationController
   def actions
   end
 end
+# class PagesController < ApplicationController
+#   skip_before_action :authenticate_user!, only: [:home]
+
+#   def home
+#     if current_user
+#       @current_budget = current_user.budgets.last
+#       if @current_budget
+#         @remaining_budget = @current_budget.remaining_budget
+#         @total_expenses = @current_budget.total_expenses
+#       else
+#         @remaining_budget = 0
+#         @total_expenses = 0
+#       end
+#     else
+#       @current_budget = nil
+#       @remaining_budget = 0
+#       @total_expenses = 0
+#     end
+#   end
+#   # def home
+#   #   # @budgets = current_user.budgets
+#   #   @current_budget = current_user.budgets.last
+#   #   @remaining_budget = @current_budget.remaining_budget
+#   #   @total_expenses = @current_budget.total_expenses
+#   # end
+
+#   def dashboard
+#     @budgets = current_user.budgets
+#     @expenses = Expense.all
+#   end
+
+#   # def actions
+#   # end
+# end
